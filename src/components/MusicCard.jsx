a64252ba96d8bd417e29e1a2d3e3e4a07a0e83f1
vitoraok1 <vitoraoki17@gmail.com>
2022-12-15 12:16:26 -0300

    adjust requirement 08

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
   state = {
